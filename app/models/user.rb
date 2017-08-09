fc35c00da3687de25b991f625f9f6edf1f65f450
Belizario Rocha <rocha.belizario@chicagobooth.edu>
2017-08-09 23:30:44 +0000

    Kick-off

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :photos
+  has_many :comments
+  has_many :likes
+  has_many :liked_photos, :through => :likes, :source => :photo
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
